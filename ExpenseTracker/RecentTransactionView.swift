a9e3841944c13ab8fcfe2401ddccaed349f609ea
DanielSarkwa <dhannysark@outlook.com>
2023-05-17 22:29:18 +0300

    Added transaction MVVM, transaction list view and data type for category

---
+++ ExpenseTracker/RecentTransactionView.swift
@@ -0,0 +1,61 @@
+//
+//  RecentTransactionView.swift
+//  ExpenseTracker
+//
+//  Created by Daniel Yeboah on 17.5.2023.
+//
+
+import SwiftUI
+
+struct RecentTransactionView: View {
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Recent Transactions")
+                    .bold()
+                
+                Spacer()
+                
+                NavigationLink {
+                    
+                } label: {
+                    HStack(spacing: 4) {
+                        Text("See All")
+                        Image(systemName: "chevron.right")
+                    }
+                    .foregroundColor(Color.text)
+                }
+            }
+            .padding(.top)
+            
+            ForEach(Array(transactionListVM.transactions.prefix(5).enumerated()), id: \.element) { index, data in
+                TransactionRow(transaction: data)
+                
+                Divider()
+                    .opacity(index == 4 ? 0 : 1)
+            }
+        }
+        .padding()
+        .background(Color.systemBackground)
+        .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .shadow(color: Color.primary.opacity(0.2), radius: 10, x: 10, y: 0)
+    }
+}
+
+struct RecentTransactionView_Previews: PreviewProvider {
+    static let transactionListVM: TransactionListViewModel = {
+        let transactionListVM = TransactionListViewModel()
+        transactionListVM.transactions = transactionListPreviewData
+        return transactionListVM
+    }()
+    
+    static var previews: some View {
+        Group {
+            RecentTransactionView()
+            RecentTransactionView()
+        }
+        .environmentObject(transactionListVM)
+    }
+}
